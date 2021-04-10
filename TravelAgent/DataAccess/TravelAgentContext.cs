93a7a284c192f450bc60c869b87a4cb1b38e376e
Hasan Assi <hasan.assi@aerdata.com>
2021-04-10 15:58:35 +0200

    Entityframework is wokring ( save to db ).

---
+++ TravelAgent/DataAccess/TravelAgentContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TravelAgent.Models;
+
+namespace TravelAgent.DataAccess
+{
+    public class TravelAgentContext : DbContext
+    {
+        public TravelAgentContext(DbContextOptions<TravelAgentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().ToTable("TodoItem");
+        }
+    }
+}
