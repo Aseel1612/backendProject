ad611349af9abcba3792a725d5288fcc4e3c882d
Hasan Assi <hasan.assi@aerdata.com>
2021-04-27 20:54:47 +0200

    Add stores + email to tourist

---
+++ TravelAgent/Migrations/20210427185307_addStores.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAgent.Migrations
+{
+    public partial class addStores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Tourist",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stores");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Tourist");
+        }
+    }
+}
--- TravelAgent/Models/Tourist.cs
+++ TravelAgent/Models/Tourist.cs
@@ -10,6 +10,7 @@ namespace TravelAgent.Models
     {
         public long Id { get; set; }
         public string FullName { get; set; }
+        public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
 
