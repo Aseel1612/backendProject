5fc4b204408c3341f038dcc84b832405316eb306
Aseel Assi <aseel.assi16@gmail.com>
2021-04-27 21:49:17 +0300

    Add stores

---
+++ TravelAgent/Controllers/StoresController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelAgent.DataAccess;
+using TravelAgent.Models;
+
+namespace TravelAgent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoresController : ControllerBase
+    {
+        private readonly TravelAgentContext _context;
+
+        public StoresController(TravelAgentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Stores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Stores>>> GetStores()
+        {
+            return await _context.Stores.ToListAsync();
+        }
+
+        // GET: api/Stores/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Stores>> GetStores(long id)
+        {
+            var stores = await _context.Stores.FindAsync(id);
+
+            if (stores == null)
+            {
+                return NotFound();
+            }
+
+            return stores;
+        }
+
+        // PUT: api/Stores/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStores(long id, Stores stores)
+        {
+            if (id != stores.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stores).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoresExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Stores
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Stores>> PostStores(Stores stores)
+        {
+            _context.Stores.Add(stores);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStores", new { id = stores.Id }, stores);
+        }
+
+        // DELETE: api/Stores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStores(long id)
+        {
+            var stores = await _context.Stores.FindAsync(id);
+            if (stores == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stores.Remove(stores);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StoresExists(long id)
+        {
+            return _context.Stores.Any(e => e.Id == id);
+        }
+    }
+}
--- TravelAgent/DataAccess/TravelAgentContext.cs
+++ TravelAgent/DataAccess/TravelAgentContext.cs
@@ -21,8 +21,12 @@ namespace TravelAgent.DataAccess
         {
             modelBuilder.Entity<TodoItem>().ToTable("TodoItem");
             modelBuilder.Entity<Tourist>().ToTable("Tourist");
+            modelBuilder.Entity<Stores>().ToTable("Stores");
+
         }
 
         public DbSet<TravelAgent.Models.Tourist> Tourist { get; set; }
+
+        public DbSet<TravelAgent.Models.Stores> Stores { get; set; }
     }
 }
